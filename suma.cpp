c504e452a6c12a3bce4da850cafe52339138d2d3
Faustomj03 <fausto.montano.jaen@utelvt.edu.ec>
2022-09-27 20:50:45 -0500

    Programas inicio  a mi repositorio git hub

---
+++ suma.cpp
@@ -0,0 +1,18 @@
+//Creado por stalin
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float x,y,z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
+ cout<<"El resutado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+}
+
